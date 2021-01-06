4cdefe293e6000352a2402489ace51478c420968
iiTONELOC <anthonytropeano@me.com>
2021-01-06 17:10:22 -0500

    object optimization

--- assets/js/game.js
+++ assets/js/game.js
@@ -187,15 +187,25 @@ var playerInfo = {
     this.attack = 10;
   },
   refillHealth: function() {
+    if (this.money >=7){
     window.alert("Refilling player's health by 20 for 7 dollars.");
     this.health += 20;
     this.money -= 7;
-  },
+  }
+  else {
+    window.alert("You dont have enough money!")
+  }
+},
   upgradeAttack: function() {
+    if (this.money >=7) {
     window.alert("Upgrading player's attack by 6 for 7 dollars.");
     this.attack += 6;
     this.money -= 7;
   }
+  else {
+    window.alert("You don't have enough money!")
+  }
+}
 };
 
 // enemy information
